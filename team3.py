f4638e88fc86c8ac8787cd773320a9a496ff647e
anthonyholmes42 <45492091+anthonyholmes42@users.noreply.github.com>
2018-12-06 08:18:05 -0600

    Update team3.py

--- team3.py
+++ team3.py
@@ -7,9 +7,16 @@
 ####
 import random
 
-team_name = 'Ur m0m g4y'
-strategy_name = 'Betray, then Collude until Betrayed, Unless they start colluding again, then collude until betrayed'
-strategy_description = 'Your Mom = "G" + "A" + "Y"'
+team_name = 'ur m0m g4y'
+strategy_name = 'spiteful_cc'
+strategy_description = 'Its really hard to explain'
+
+def countB(history):
+    sum = 0
+    for element in history:
+        if element == 'b':
+            sum += 1
+    return sum
     
 def move(my_history, their_history, my_score, their_score):
     # tit for tat, until betrayed twice in a row, then always betray
@@ -17,25 +24,66 @@ def move(my_history, their_history, my_score, their_score):
     # winner12: c, c then pavlov, then if betrayed twice, betray
     #pavlov: c on the first move, then betrays only if the players did not agree
     # gradual: c on first move, betray n times after nth bertayal, after two colludes, start colluding again
-   
-    betrayed = False
     
-    if 'b' in their_history:
-        betrayed = True
-    else:
+    '''tft_spiteful'''
+    if strategy_name == 'tft_spiteful': 
+        if 'bb' in their_history:
+            return 'b'
+        elif len(their_history) >= 1:
+            return their_history[-1]
+        else:
+            return 'c'
+    
+    '''spiteful_cc'''
+    if strategy_name == 'spiteful_cc':
+        if len(my_history) <= 1:
+            return 'c'
+        elif 'b' not in their_history:
+            return 'c'
+        else:
+            return 'b'
+   
+    '''winner12'''
+    if strategy_name == 'winner12':
         betrayed = False
         
-    if len(their_history) > 2:
-        if their_history[-2:-1]+their_history[-1] == 'cc':
-            betrayed = False
-
+        if countB(their_history) >= 2:
+            betrayed = True
+        
+        if len(my_history) < 2:
+            return 'c'
+        elif not betrayed:
+            if their_history[-1] == my_history[-1]:
+                return 'c'
+            if their_history[-1] != my_history[-1]:
+                return 'b'
+        elif betrayed:
+            return 'b'
+        
+    '''gradual'''
+    if strategy_name == 'gradual':
+        num = 0
+        
+        if len(their_history) >= 1:
+            if their_history[-1] == 'b':
+                num = countB(their_history)
+                
+        if len(their_history) >= 2:
+            if their_history[-2] + their_history[-1] == 'cc':
+                num = 0
+                
+        if num == 0:
+            return 'c'
+        else:
+            return 'b'
+            num -= 1
+            
+    '''spiteful'''
+    if strategy_name == 'spiteful':
+        if 'b' in their_history:
+            return 'b'
+        else:
+            return 'c'
     
-    if len(my_history) == 0:
-        return 'b'
-    elif not betrayed:
-        return 'c'
-    elif betrayed:
-        return 'b'
-    else:
-        return random.choice('b', 'c')
+
     
