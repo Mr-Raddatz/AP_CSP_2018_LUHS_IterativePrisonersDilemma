0879d62f38c73dea518dc64c83f701ec7bc48a6b
Shanekidd <45491851+Shanekidd@users.noreply.github.com>
2018-12-07 09:02:11 -0600

    Update team0.py

--- team0.py
+++ team0.py
@@ -4,7 +4,6 @@
 #     strategy_name: a string
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
-#### LUKE IS COOL SHANE IS COOL
 
 team_name = 'Fortnight' # Only 10 chars displayed.
 strategy_name = 'Random random'
@@ -13,7 +12,7 @@ strategy_description = 'Random^2'
 import random
 
 a = random.randint(0,1)
-
+def move(my_history, thei_history, my_score, their_score):
 if a == 0:
     return 'b'
 if a == 1:
